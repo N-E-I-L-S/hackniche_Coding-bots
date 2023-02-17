994722f20e9f3586539b664f6b8dae458fbd7728
Neil Shukla <neilpshukla221203@gmail.com>
2023-02-17 11:29:40 +0530

    Skeleton made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { ErrorPage } from './pages/ErrorPage';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import Login from './pages/Login';
+import Landing from './pages/LandingPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import { About } from './pages/About';
+import { Services } from './pages/Services';
+import { Pricing } from './pages/Pricing';
+import { Contact } from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-2">
+      <Router>
+        <Navbar />
+        <div className='min-h-screen'>
+          <Routes>
+
+            <Route path='/' element={<Landing />} />
+            <Route path='/home' element={<ProtectedRoute page={<Home />} />} />
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/services" element={<Services />}></Route>
+            <Route path="/pricing" element={<Pricing />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="*" element={<ErrorPage />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+
     </div>
   );
 }
