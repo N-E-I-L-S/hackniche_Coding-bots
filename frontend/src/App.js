11c67c0367670e5e4d1911a4249883cea6838e24
Neil Shukla <neilpshukla221203@gmail.com>
2023-02-17 11:59:46 +0530

    Login utility added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { ErrorPage } from './pages/ErrorPage';
-import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer';
-import Login from './pages/Login';
-import Landing from './pages/LandingPage';
+import Home  from './pages/Home';
+import  ErrorPage  from './pages/ErrorPage';
+// import { Navbar } from './components/Navbar';
+// import { Footer } from './components/Footer';
+import Login from './pages/login/Login';
+import Landing from './pages/Landing';
 import ProtectedRoute from './components/ProtectedRoute';
-import { About } from './pages/About';
-import { Services } from './pages/Services';
-import { Pricing } from './pages/Pricing';
-import { Contact } from './pages/Contact';
+
 
 function App() {
   return (
     <div className="p-2">
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
         <div className='min-h-screen'>
           <Routes>
 
             <Route path='/' element={<Landing />} />
-            <Route path='/home' element={<ProtectedRoute page={<Home />} />} />
+            {/* <Route path='/home' element={<ProtectedRoute page={<Home />} />} /> */}
+            <Route path="/home" element={<Home />}></Route>
             <Route path="/login" element={<Login />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/services" element={<Services />}></Route>
-            <Route path="/pricing" element={<Pricing />}></Route>
-            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            
             <Route path="*" element={<ErrorPage />}></Route>
           </Routes>
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
 
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Home() {
   return (
+    <>
     <div>Home</div>
+    <NavLink to='/login'> Login </NavLink>
+    </>
   )
 }
